2292c3d92ca81f114e5bc6a6479dcfe9a4788ac0
Asad Lion <mabdiaziz416@gmail.com>
2022-12-09 20:02:00 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Change Background Color Function//
+function changeBgColor(bgColor){
+    var body = document.getElementById('background');
+    body.style.backgroundColor = bgColor.value;
+}
+
+//Change Font Color Function//
+function changeFontColor(fontColor){
+    var body = document.getElementById('background');
+    body.style.color = fontColor.value;
+} 
+
